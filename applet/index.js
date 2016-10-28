f8611ced47fdd3c26f8da63145aea982d8f157f2
Jan Unnold <j.unnold@gmx.de>
2016-10-28 14:12:50 +0200

    Added loading indicator

--- applet/index.js
+++ applet/index.js
@@ -2,15 +2,16 @@ angular.module('applet', [ 'ng' ])
 
   .run(function($rootScope, $http) {
 
-    $http.get('https://api.github.com/users/janunld').then(function(res) {
-      $rootScope.$my = res.data;
+    // Holds several indicators for loading states of different components
+    $rootScope.loading = {};
 
-      /* TODO: Choose if this is pretty or not?
-      angular.element('.ju.profile-avatar').tooltip({
-        container: '.ju.profile',
-        title: $rootScope.$my.bio
-      });*/
+    $rootScope.loading.userData = true;
+    $http.get('https://api.github.com/users/janunld').then(function(res) {
+      $rootScope.loading.userData = false;
+
+      $rootScope.$my = res.data;
     }).catch(function(res) {
+      $rootScope.loading.userData = false;
       $rootScope.$$error = res;
     });
 
