af15c822192d36e35c681d0031b676bdcf31aa5d
Jan Unnold <j.unnold@gmx.de>
2024-09-24 23:20:51 +0200

    Experience duration format

--- janunld.github.io/src/app/components/experience.component.ts
+++ janunld.github.io/src/app/components/experience.component.ts
@@ -5,6 +5,7 @@ import { COLOR_SCHEME } from '../common';
 export interface Company {
   readonly id: string;
   displayName: string;
+  type: 'GmbH' | 'AG' | string;
   logoSrc: {
     default: string;
     dark: string;
@@ -48,6 +49,11 @@ export interface Experience {
                 {{ experience.company.displayName }}
               </div>
             }
+            <span
+              class="ml-2.5 px-2 py-0.5 text-xs text-neutral-400 dark:text-neutral-600 border-2 border-neutral-200 dark:border-neutral-800 rounded-full"
+            >
+              {{ experience.company.type }}
+            </span>
           </a>
           <a
             class="ml-4 text-sm flex items-center decoration-1 max-sm:hidden"
@@ -74,7 +80,7 @@ export interface Experience {
             </div>
             <div class="my-3">
               <div>{{ position.title }}</div>
-              <div class="inline-block mt-1 me-2 text-xs text-neutral-500">
+              <div class="inline-block mt-1 text-xs text-neutral-500">
                 <span>{{ position.startDate | date: dateFormat() }}</span>
                 <span class="mx-1">&dash;</span>
                 @if (position.endDate) {
@@ -82,7 +88,29 @@ export interface Experience {
                 } @else {
                   <span>now</span>
                 }
+                <span class="mx-2 text-neutral-400 dark:text-neutral-600">
+                  &centerdot;
+                </span>
               </div>
+              @if (getDuration(position); as duration) {
+                <div
+                  class="inline-block text-xs text-neutral-400 dark:text-neutral-600"
+                >
+                  @if (duration.years) {
+                    <span class="first:mr-1">{{
+                      duration.years ? duration.years + 'y' : ''
+                    }}</span>
+                  }
+                  @if (duration.months) {
+                    <span>{{
+                      duration.months ? duration.months + 'm' : ''
+                    }}</span>
+                  }
+                  <span class="mx-2 text-neutral-400 dark:text-neutral-600"
+                    >&centerdot;</span
+                  >
+                </div>
+              }
               @if (position.description) {
                 <div
                   class="inline-block text-xs text-neutral-400 dark:text-neutral-600"
@@ -105,32 +133,35 @@ export class ExperienceComponent {
   readonly companies = input<Company[]>([
     {
       id: 'nt',
-      displayName: 'Novatec Consulting GmbH',
+      displayName: 'Novatec Consulting',
       href: 'https://www.novatec-gmbh.de/',
       logoSrc: {
         default: '/img/novatec.svg',
         dark: '/img/novatec-dark.svg',
       },
       hideDisplayName: true,
+      type: 'GmbH',
     },
     {
       id: 'vg',
-      displayName: 'vitagroup AG',
+      displayName: 'vitagroup',
       href: 'https://www.vitagroup.ag/',
       logoSrc: {
         default: '/img/vitagroup.svg',
         dark: '/img/vitagroup-dark.svg',
       },
       hideDisplayName: true,
+      type: 'AG',
     },
     {
       id: 'mgs',
-      displayName: 'mg.softech GmbH',
+      displayName: 'mg.softech',
       href: 'https://www.mgsoftech.de/',
       logoSrc: {
         default: '/img/mgsoftech.svg',
         dark: '/img/mgsoftech-dark.svg',
       },
+      type: 'GmbH',
     },
   ]);
   readonly experiences = input<Experience[]>([
@@ -156,15 +187,15 @@ export class ExperienceComponent {
     },
     {
       title: 'Software Developer',
-      startDate: '2016-06-01',
+      startDate: '2017-06-01',
       endDate: '2018-12-31',
       companyId: 'mgs',
       description: 'full-time',
     },
     {
       title: 'Software Developer',
-      startDate: '2017-06-01',
-      endDate: '2015-06-01',
+      startDate: '2015-06-01',
+      endDate: '2017-06-01',
       companyId: 'mgs',
       description: 'full-time, apprenticeship',
     },
@@ -179,4 +210,31 @@ export class ExperienceComponent {
       ),
     })),
   );
+
+  getDuration({ startDate, endDate }: Experience) {
+    const start = new Date(startDate);
+    const end = endDate ? new Date(endDate) : new Date();
+
+    const isSameYear = end.getFullYear() === start.getFullYear();
+
+    const startMonths = 11 - start.getMonth();
+    const endMonths = end.getMonth();
+    const inBetweenMonths =
+      Math.max(end.getFullYear() - start.getFullYear() - 1, 0) * 11;
+    let fullMonths = 0;
+
+    if (inBetweenMonths > 0 || !isSameYear) {
+      fullMonths = startMonths + inBetweenMonths + endMonths;
+    } else {
+      fullMonths = endMonths;
+    }
+
+    const years = Math.floor(fullMonths / 11);
+    const months = fullMonths % 11;
+
+    return {
+      years,
+      months,
+    };
+  }
 }
